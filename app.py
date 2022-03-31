8e2953fbe505480220a755cbbc7187b40355db34
vharikar <vibha_harikar@intuit.com>
2022-03-31 20:58:52 +0000

    Fix more bugs

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ app.layout = html.Div(children=[
               [Input('breed-drop', 'value')])
 def on_select(breed):
    
-   breed_attrs_df = cats_df[cats_df['id']==breed][['adaptability',    'affection_level','child_friendly','dog_friendly','energy_level','grooming','health_issues','intelligence','shedding_level','social_needs','stranger_friendly']]
+   breed_attrs_df =  cats_df[cats_df['id']==breed[['adaptability','affection_level','child_friendly','dog_friendly','energy_level','grooming','health_issues','intelligence','shedding_level','social_needs','stranger_friendly']]
 
    ratings_df = get_ratings_fig(breed_attrs_df)
    fig = ratings_df.plot(kind="barh")
